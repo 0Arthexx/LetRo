9d9ca8d199dce3248b0d56304a8585d81e499bcb
Alvon Jovanus Chandra <alvonchan@gmail.com>
2022-10-10 18:13:17 +0700

    add parallax background

---
+++ script/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const background = document.getElementById('background');
+const gunung = document.getElementById('gunung');
+const daun1 = document.getElementById('daun1');
+const daun2 = document.getElementById('daun2');
+const daun3 = document.getElementById('daun3');
+const buwong = document.getElementById('buwong');
+
+window.addEventListener('scroll', function () {
+   let value = window.scrollY;
+
+   gunung.style.top = `${value * 0.8}px`;
+   daun1.style.top = `${value * 0.55}px`;
+   daun2.style.top = `${value * 0.4}px`;
+   daun3.style.top = `${value * 0}px`;
+   buwong.style.left = `${value * 1.05}px`;
+   console.log(value);
+});
